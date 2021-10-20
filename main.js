82dc4cf825fb94983c37be648d79f64b0fe68f93
Fernie Qin <yiq028@ucsd.edu>
2021-10-19 22:02:35 -0400

    first commit, everthing added

--- main.js
+++ main.js
@@ -1,27 +1,122 @@
-import * as d3 from "d3";
-import vegaEmbed from "vega-embed";
+// (function () {
+//   var margin = {top:50, left:50, right:50, bottom:50},
+//   height = 400 - margin.top - margin.bottom,
+//   width = 800 - margin.left - margin.right;
 
-d3.select("#d3-div").append("p").text("hello from D3");
+//   var svg = d3.select("#map")
+//   .append("svg")
+//   .attr("height", height + margin.top + margin.bottom)
+//   .attr("width", width + margin.left + margin.right)
+//   .append("g")
+//   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-vegaEmbed("#vega-div", {
-  $schema: "https://vega.github.io/schema/vega-lite/v5.json",
-  description: "A simple bar chart with embedded data.",
-  data: {
-    values: [
-      { a: "A", b: 28 },
-      { a: "B", b: 55 },
-      { a: "C", b: 43 },
-      { a: "D", b: 91 },
-      { a: "E", b: 81 },
-      { a: "F", b: 53 },
-      { a: "G", b: 19 },
-      { a: "H", b: 87 },
-      { a: "I", b: 52 },
-    ],
-  },
-  mark: "bar",
-  encoding: {
-    x: { field: "a", type: "nominal", axis: { labelAngle: 0 } },
-    y: { field: "b", type: "quantitative" },
-  },
-});
+//   /***
+//    * read in the world.topojson
+//    * read in the poverty.json
+//    */
+//   d3.queue()
+//   .defer(d3.json, "world.topojson")
+//   .defer(d3.csv, "poverty.csv")
+//   .await(ready)
+
+
+//   /**
+//    * create a new projection using mercator and center it(translate)
+//    * and zoom in a certain amount(scale)
+//    */
+//   var offset = [width / 1.8, height / 2]
+//   var projection = d3.geoMercator()
+//   .translate(offset)
+//   .scale(100)
+
+//   /** 
+//    * create a path(geopath) using a projection
+//   */
+//   var path = d3.geoPath()
+//   .projection(projection)
+
+
+//   var maxCountryList = []
+//   var lastCountry;
+//   var number = 0;
+//   var currentCountryName = "";
+
+// function ready (error, data, poverty) {
+//   console.log(data)
+
+//   var countries = topojson.feature(data, data.objects.countries).features
+
+//   console.log(countries)
+
+
+//   var countryNames = {}
+//   // poverty.forEach(function(d) {
+//   //   countryNames[d.id] = d.name
+//   //   option = countryList.append("option");
+//   //   option.text(d.name);
+//   //   option.property("value", d.id);
+//   // })
+
+//   var current;
+
+//   console.log(poverty)
+//   /**
+//    * add a path for each country (shape --> path)
+//    */
+//   svg.selectAll(".country")
+//   .data(countries)
+//   .enter().append("path")
+//   .attr("class", "country")
+//   .attr("d", path)
+//   .on("click", function(d) {
+
+//     number += 1
+//     if (number == 1) {
+//       current = this;
+//       d3.select(current).classed("selected",true)
+//       firstCountryName = d.properties.name
+//       console.log(firstCountryName)
+//       document.getElementById("country_code_1").innerHTML = "The 1st country selected is: " + firstCountryName;
+//     }
+//     else if (number == 2) {
+//       current = this;
+//       d3.select(current).classed("selected",true)
+//       secondCountryName = d.properties.name
+//       console.log(secondCountryName)
+//       document.getElementById("country_code_2").innerHTML = "The 2nd country selected is: " + secondCountryName;
+//     }
+//     else {
+//       alert("You have already selected two countries. Page will be reloaded.")
+//       location.reload();
+//       return false;
+//     }
+//   })
+// }
+
+// }) ();
+
+
+// if (!current) {
+//   if (maxCountryList.length == 2) {
+//     //max two countries and pop the last country
+//     lastCountry = maxCountryList.pop()
+//     d3.select(lastCountry).classed("selected",false);
+//     current = this;
+//     d3.select(current).classed("selected",true);
+//     maxCountryList.push(current)
+//     console.log(maxCountryList.length)
+//   } 
+//   else {
+//     //current = this;
+//     d3.select(this).classed("selected",true);
+//     maxCountryList.push(this)
+//     console.log(maxCountryList.length)
+//   }
+// } 
+// else {
+//   d3.select(current).classed("selected",false);
+//   maxCountryList.pop()
+//   current = undefined;
+//   // number -= 1;
+//   console.log(maxCountryList.length)
+// }
